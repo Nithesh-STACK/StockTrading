ab796e105bf527a7dbbd2f36260ef7e76431d53d
Nithesh S <nitheshvilla@gmail.com>
2021-12-19 12:57:11 +0530

    5th commit

--- app/src/main/java/com/example/stocktrading/OtherUserAdapterClass.kt
+++ app/src/main/java/com/example/stocktrading/OtherUserAdapterClass.kt
@@ -28,13 +28,15 @@ class OtherUserAdapterClass(var UsersVal:MutableList<otherUserDataClass>, var co
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val otherUserEmail = itemView.findViewById<TextView>(R.id.otheruserEmail)
         val ownsStock = itemView.findViewById<TextView>(R.id.ownsStockText)
-
+        val ownEmail=itemView.findViewById<TextView>(R.id.textView6)
 
     }
 
     override fun onBindViewHolder(holder: OtherUserAdapterClass.ViewHolder, position: Int) {
         holder.otherUserEmail.text="${UsersVal[position].email}"
         holder.ownsStock.text = "Owns stocks: ${UsersVal[position].ownsStocks}"
-
+        val emailId=UsersVal[position].email.toString()
+        val photoText = emailId.substring(0, 1)?.toUpperCase()
+        holder.ownEmail.text=photoText
     }
 }
